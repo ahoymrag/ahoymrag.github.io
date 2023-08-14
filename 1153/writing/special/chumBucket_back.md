3c96502b01164049d626845859f835f3485c5ca6
agSuperCoder123 <alex@ahoy.ooo>
2023-08-14 00:09:20 -0400

    updated the chum bucket

--- 1153/writing/special/chumBucket_back.md
+++ 1153/writing/special/chumBucket_back.md
@@ -5,4 +5,39 @@ back from jfk and it's been a horizontal
 
 # back 
 
-8.13.23 - said good bye - after seieng eachother for nearly 17 days every day - it has been a wild ride
+7.27 - thursday - ellen came back to the USA!
+
+7.28 - friday - cuddle and adventure and beach and errands around new haven 
+
+7.29 - saturday - initial move
+
+7.30 - sunday - help move -ellen living at beach house
+
+7.31 - monday - move jojo - beach house - errands and grilling
+
+8.01.23 - Move day - beach house
+
+8.02.23 - Wednesday Night - Help Move
+
+8.03.23 - Lifegroup walk around wooster
+
+8.04.23 - Friday hanging out?
+
+8.05.23 - Cuddle All Day Day - brendan and josh playwright 
+
+8.06.23 - Brew Day
+
+8.07.23 - Rachels
+
+8.08.23 - Cod Climbing - Archie Moores
+
+8.09.23 - Beach house - leah - 
+
+8.10.23 - Rudies - Lifegroup
+
+8.11.23 - Friday night out in town - 
+
+8.12.23 - # went to openheinger - went to buffalo wild wings - went to hm - went to gamestop - went to hot topic - went around the CT mall - went back - thunderstorms and walks and working on dallas movie script and storyboard - before and after napping
+
+8.13.23 - said good bye - after seieng eachother for nearly 17 days every day - it has been a wild ride 
+# 8.13.23 - hung out outside at the yale school of mgmt
---
+++ experiments/svelte-dashboard/src/App.svelte
@@ -0,0 +1,117 @@
+<script>
+	import { slide, scale, fade, push, pull } from "svelte/transition";
+  
+	let currentTab = 'Dashboard';
+</script>
+
+<style>
+	body {
+		font-family: Arial, sans-serif;
+		margin: 0;
+		padding: 0;
+		display: flex;
+		flex-direction: column;
+		height: 100vh;
+	}
+
+	header {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		padding: 10px 20px;
+		background-color: #f4f4f4;
+		border-bottom: 1px solid #e1e1e1;
+	}
+
+	.menu {
+		display: flex;
+		gap: 20px;
+	}
+
+	.menu a {
+		text-decoration: none;
+		color: black;
+	}
+
+	.menu a:hover {
+		text-decoration: underline;
+	}
+
+	.dashboard {
+		flex: 1;
+		display: flex;
+		justify-content: space-between;
+		align-items: flex-end;
+		padding: 0 20px 20px;
+	}
+
+	.dashboard > button {
+		padding: 20px;
+		font-size: 20px;
+		border: none;
+		background-color: #333;
+		color: #fff;
+		cursor: pointer;
+		border-radius: 8px;
+		transition: 0.3s;
+	}
+
+	.dashboard > button:hover {
+		background-color: #555;
+	}
+
+	.content {
+		flex: 2;
+		background-color: #f9f9f9;
+		padding: 20px;
+		text-align: center;
+		overflow: hidden;
+	}
+
+	.tab {
+		margin: auto;
+		padding: 20px;
+		background-color: #fff;
+		border-radius: 8px;
+		box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.1);
+		width: 100%;
+		max-width: 800px;
+		text-align: center;
+	}
+</style>
+
+<header>
+	<div>
+		<img src="apple-logo.png" alt="Apple logo" width="40" height="40">
+	</div>
+	<nav class="menu">
+		<a href="#" on:click={() => currentTab = 'Dashboard'}>Dashboard</a>
+		<a href="#" on:click={() => currentTab = 'Settings'}>Settings</a>
+		<a href="#" on:click={() => currentTab = 'Help'}>Help</a>
+	</nav>
+</header>
+
+<main class="dashboard">
+	<button on:click={() => currentTab = 'Dashboard'} transition:push={{delay: 200, dir: 1}}>Dashboard</button>
+	<button on:click={() => currentTab = 'Settings'} transition:push={{delay: 200, dir: 1}}>Settings</button>
+	<button on:click={() => currentTab = 'Help'} transition:push={{delay: 200, dir: 1}}>Help</button>
+</main>
+
+<div class="content" transition:pull={{delay: 300, dir: -1}}>
+	{#if currentTab === 'Dashboard'}
+		<div class="tab">
+			<h3>Dashboard</h3>
+			<p>Welcome to your dashboard. Here you can find a summary of your activities, notifications, and updates.</p>
+		</div>
+	{:else if currentTab === 'Settings'}
+		<div class="tab">
+			<h3>Settings</h3>
+			<p>Manage your preferences, security settings, and personalize your dashboard experience.</p>
+		</div>
+	{:else}
+		<div class="tab">
+			<h3>Help</h3>
+			<p>Need assistance? Browse our FAQs, watch tutorials, or contact our support team.</p>
+		</div>
+	{/if}
+</div>
