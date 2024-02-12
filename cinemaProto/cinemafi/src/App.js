fcccc081ae41bc873936fac74f9489971fb13f77
agSuperCoder123 <alex@ahoy.ooo>
2024-02-12 16:30:45 -0500

    added react and reactive native for cinemafi

---
+++ cinemaProto/cinemafi/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to CinemaFi</h1>
+        <p>Indie Film Funding on Blockchain</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
