816271f46c1139a043d17d3f4e7cfd61c405d521
agSuperCoder123 <alex@ahoy.ooo>
2024-02-12 20:28:56 -0500

    updated and rerouted some things

--- cinemaClean/cinema-fi-app/src/App.vue
+++ cinemaClean/cinema-fi-app/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <div class="content">
+      <TheSidebar /> <!-- Moved Sidebar component here -->
       <img alt="Vue logo" src="./assets/A_cinemaFI.png" class="logo">
      
       <UserDashboard /> <!-- Correctly referencing the updated component name -->
@@ -13,12 +14,14 @@
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import UserDashboard from './components/UserDashboard.vue' // Correctly importing the renamed component
+import TheSidebar from './components/TheSidebar.vue' // Import TheSidebar component here
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
-    UserDashboard // Correctly registering the renamed component
+    UserDashboard, // Correctly registering the renamed component
+    TheSidebar // Register TheSidebar component here
   }
 }
 </script>
--- cinemaClean/cinema-fi-app/src/components/UserDashboard.vue
+++ cinemaClean/cinema-fi-app/src/components/UserDashboard.vue
@@ -1,6 +1,5 @@
 <template>
     <div class="user-dashboard">
-      <TheSidebar /> <!-- Added Sidebar component -->
       <h1>User Dashboard</h1>
       <p>Welcome to your User Dashboard. Here you can view your app's analytics and manage content.</p>
       <section id="left-dashy">
@@ -14,13 +13,8 @@
 </template>
 
 <script>
-import TheSidebar from './TheSidebar.vue'; // Import TheSidebar component
-
 export default {
   name: 'UserDashboard', // Updated component name to be multi-word
-  components: {
-    TheSidebar, // Register TheSidebar component
-  },
 }
 </script>
 
