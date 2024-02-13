61086692c6bc00a02bf7ec61b45dcbff59c8cad2
agSuperCoder123 <alex@ahoy.ooo>
2024-02-12 20:24:32 -0500

    added basic sidebar

--- cinemaClean/cinema-fi-app/src/components/TheSidebar.vue
+++ cinemaClean/cinema-fi-app/src/components/TheSidebar.vue
@@ -1,17 +1,18 @@
 <template>
     <div class="sidebar">
       <div class="app-sidebar">
+        This is where the sidebar will go
         <a href="" class="app-sidebar-link active">
-          <i class="fas fa-home"></i>
+          <i class="fas fa-home">Home</i>
         </a>
         <a href="" class="app-sidebar-link">
-          <i class="fas fa-chart-pie"></i>
+          <i class="fas fa-chart-pie">Finances</i>
         </a>
         <a href="" class="app-sidebar-link">
-          <i class="fas fa-calendar-alt"></i>
+          <i class="fas fa-calendar-alt">Calendar</i>
         </a>
         <a href="" class="app-sidebar-link">
-          <i class="fas fa-cog"></i>
+          <i class="fas fa-cog">Settings</i>
         </a>
       </div>
     </div>
