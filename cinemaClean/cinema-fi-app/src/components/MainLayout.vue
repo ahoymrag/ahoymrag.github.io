771b3c53040a8220f0c2ef4e1bce5a9783d8a89a
agSuperCoder123 <alex@ahoy.ooo>
2024-02-12 20:02:31 -0500

    added router

---
+++ cinemaClean/cinema-fi-app/src/components/MainLayout.vue
@@ -0,0 +1,31 @@
+<!-- src/components/MainLayout.vue -->
+<template>
+  <div class="main-layout">
+    <TheSidebar />
+    <div class="content-area">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script>
+import TheSidebar from './TheSidebar.vue';
+
+export default {
+  components: {
+    TheSidebar
+  }
+}
+</script>
+
+<style scoped>
+.main-layout {
+  display: flex;
+  height: 100vh;
+}
+
+.content-area {
+  flex-grow: 2;
+  padding: 20px; /* Adjust as needed */
+}
+</style>
---
+++ cinemaClean/cinema-fi-app/src/components/TheSidebar.vue
@@ -0,0 +1,16 @@
+<!-- Sidebar.vue -->
+<template>
+    <div class="sidebar">
+      <!-- Sidebar content -->
+    </div>
+  </template>
+  
+  <style scoped>
+  .sidebar {
+    /* Sidebar styles (width, colors, etc.) */
+    width: 33.33%;
+    background-color: var(--link-color-active-bg);
+    /* ... */
+  }
+  </style>
+  
